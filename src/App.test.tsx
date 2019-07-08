c2d121da5be9175af5865e4a2a70f46c98cf17d7
Noah Potter <npotter@frontsteps.com>
2019-07-08 17:37:41 -0600

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import td from "testdouble";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+interface DogOptions {
+  color: string;
+}
+
+class AnimalBuilder {
+  // Doesn't matter
+  configure(options: any): AnimalBuilder {
+    return this;
+  }
+
+  makeNoise() {}
+}
+
+class DogBuilder {
+  constructor(animalBuilder: AnimalBuilder, dogOptions: DogOptions) {
+    animalBuilder.configure(dogOptions).makeNoise();
+  }
+}
+
+test("test DogBuilder", () => {
+  const animalBuilder = td.object<AnimalBuilder>();
+  const dogOptions = td.object<DogOptions>();
+
+  // Non-working version
+  // td.when(animalBuilder.configure(dogOptions)).thenReturn({
+  //   makeNoise: td.func()
+  // });
+
+  // Working version
+  // td.when(
+  //   animalBuilder.configure(
+  //     td.matchers.argThat((options: any) => {
+  //       return options === dogOptions;
+  //     })
+  //   )
+  // ).thenReturn({
+  //   makeNoise: td.func()
+  // });
+
+  new DogBuilder(animalBuilder, dogOptions);
 });
